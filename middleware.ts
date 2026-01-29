b9e6f5d76b9dd6a991ab619054d551ec4e7ab99f
Taha_Essaouir <154090928+Taha-Essaouir@users.noreply.github.com>
2026-01-29 23:16:29 +0100

    build

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,27 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  '/transactions(.*)',
-  '/settings(.*)'
-])
+// Hadu homa les pages li ay wa7d y-qdr y-choufhom (Public)
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+  '/api/webhooks(.*)' // Darouri bach l-API ma t-crashich
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
+  // Ila ma kantch page public, 7miha (Protect)
+  if (!isPublicRoute(req)) {
     const { userId } = await auth();
     if (!userId) {
-      return Response.redirect('/sign-in');
+      return Response.redirect("/sign-in");
     }
   }
-})
+});
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
+  matcher: [
+    // Skip Next.js internals and all static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
